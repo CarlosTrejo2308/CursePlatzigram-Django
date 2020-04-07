4c55c73f6a956e2e3f4fcfc8df45c6fd3bde47c1
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2020-04-06 21:35:26 -0500

    Admin Dashboard

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
 
+from users.models import Profile
+
 # Register your models here.
+#admin.site.register(Profile)
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    #Profile admin
+
+    #Change the order on how it's shown
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+
+    #Define what values re clickable
+    list_display_links = ('pk', 'user')
+
+    #What values can be easily editable
+    list_editable = ('phone_number', 'website', 'picture')
+
+    #Where to search
+    search_fields = (
+        'user__email',
+        'user__first_name',
+        'user__last_name',
+        'phone_number'
+    )
+
+    #Django filters
+    list_filter = (
+        'created',
+        'modified',
+        'user__is_active',
+        'user__is_staff'
+    )
