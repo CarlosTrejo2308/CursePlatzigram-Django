70185a27f6da74b66c57dd1702de3f182ebc2a48
Carlos N. Trejo <CarlosTrejo2308@users.noreply.github.com>
2020-04-07 19:43:45 -0500

    Update admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 
 from users.models import Profile
 
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from django.contrib.auth.models import User
+
 # Register your models here.
 #admin.site.register(Profile)
 
@@ -33,3 +37,48 @@ class ProfileAdmin(admin.ModelAdmin):
         'user__is_active',
         'user__is_staff'
     )
+
+    # How to show a user
+    # Category - Data
+    #Its managed by tuples
+    fieldsets = (
+        ('Profile', {
+            'fields': (('user', 'picture'),),
+        }),
+        ('Extra info', {
+            'fields': (
+                ('website', 'phone_number'),
+                ('biography')
+            ),
+        }),
+        ('Metadata', {
+                'fields': (('created', 'modified'),)
+            }),
+    )
+
+    #This fields cannot be modified
+    readonly_fields = ('created', 'modified', 'user')
+
+#Creates the section to add
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profiles'
+
+#Add user admin to base user admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine,)
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_active',
+        'is_staff'
+    )
+
+admin.site.unregister(User)
+
+#Modelo - Clase
+#Adds more to create a user
+admin.site.register(User, UserAdmin)
